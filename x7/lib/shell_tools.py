90edb67a7605a4e9d44b65d2eddace38922cd600
Glenn Gribble <git@gribbelus.com>
2021-01-10 11:54:14 -0500

    Make shell tools have decent repr() like help or quit

--- x7/lib/shell_tools.py
+++ x7/lib/shell_tools.py
@@ -5,8 +5,25 @@
 """
 
 from pprint import pp
+from x7.lib.shell_tools_load import ShellTool
 
-__all__ = ['tools', 'Dir', 'pp']
+__all__ = ['tools', 'Dir', 'pp', 'help']
+
+orig_help = help
+
+
+# noinspection PyShadowingBuiltins
+def help(*args, **kwargs):
+    """Type help() for interactive help, or help(object) for help about object."""
+
+    thing = args[0] if args else None
+    if isinstance(thing, ShellTool):
+        if thing.action == help:
+            orig_help(orig_help)
+        else:
+            orig_help(thing.action)
+    else:
+        orig_help(*args, *kwargs)
 
 
 def tools():
@@ -16,7 +33,7 @@ def tools():
     print('x7 shell: %d tools from %s' % (len(loaded_tools), ', '.join('x7.%s' % lm for lm in loaded_modules)))
     width = max(map(len, loaded_tools.keys()))
     for name, val in sorted(loaded_tools.items(), key=lambda item: item[0].lower()):
-        print('  %*s - %s' % (-width, name, val.__doc__))
+        print('  %*s - %s' % (-width, name, val.doc()))
 
 
 # noinspection PyPep8Naming
